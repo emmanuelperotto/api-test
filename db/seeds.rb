e5d928e5304fcb2077119ef3cd3ca0403a8b96a6
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-10-16 14:03:49 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,43 @@
 # frozen_string_literal: true
 
-# Criando eventos
+mane_garrincha_location = {
+  lat: -15.78347475,
+  lng: -47.89920526355173
+}
+
+# Creating Events
+Event.create!(
+  name: 'HELLouWIN #8',
+  description: 'A maior festa à fantasia de Brasília',
+  lat: mane_garrincha_location[:lat],
+  lng: mane_garrincha_location[:lng]
+)
+
+Event.create!(
+  name: 'Roberto Carlos',
+  description: 'Show do Roberto Carlos no Estádio Nacional Mané Garrincha',
+  lat: mane_garrincha_location[:lat],
+  lng: mane_garrincha_location[:lng]
+)
+
+Event.create!(
+  name: 'Martin Garrix',
+  description: 'Show do Martin Garrix no Estádio Nacional Mané Garrincha',
+  lat: mane_garrincha_location[:lat],
+  lng: mane_garrincha_location[:lng]
+)
+
+# Creating Users
+User.create!(
+  name: 'Emmanuel Perotto',
+  email: 'emmanuelperotto@gmail.com',
+  password: '12345678',
+  password_confirmation: '12345678'
+)
+
+User.create!(
+  name: 'João Santos',
+  email: 'joao.santos@teste.com',
+  password: '12345678',
+  password_confirmation: '12345678'
+)
