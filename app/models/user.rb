1774201df7c440e62dbee63952d87c0d38eaf2a6
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-10-16 13:22:45 -0300

    add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@
 
 # User model
 class User < ApplicationRecord
-  # Fields
   has_secure_password
+
+  # Associations
+  has_many :comments, dependent: :nullify
+  has_many :events, through: :comments
+  has_many :reports, dependent: :nullify
 
   # Validations
   validates :name, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { is_expected.to validate_presence_of :name }
-  it { is_expected.to validate_presence_of :email }
-  it { is_expected.to validate_presence_of :password }
-  it { is_expected.to validate_confirmation_of :password }
+  context 'when testing validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+    it { is_expected.to validate_confirmation_of :password }
 
-  it { is_expected.to have_secure_password }
+    it { is_expected.to have_secure_password }
+  end
+
+  context 'when testing associations' do
+    it { is_expected.to have_many(:comments).dependent(:nullify) }
+    it { is_expected.to have_many(:events).through(:comments) }
+    it { is_expected.to have_many(:reports).dependent(:nullify) }
+  end
 end
