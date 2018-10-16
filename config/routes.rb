39b4566b62ae8f168325289f465be87cdc728a2b
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-10-16 13:44:04 -0300

    add version 1 to api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :comments
-  resources :events
+  namespace :api do
+    api_version(module: 'V1', path: { value: 'v1' }) do
+      resources :comments
+      resources :events
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
